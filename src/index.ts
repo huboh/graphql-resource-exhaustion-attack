b3848f44caa21088bccaf9b876adabcb5a5e4655
huboh <72440222+huboh@users.noreply.github.com>
2023-05-01 16:15:44 +0100

    add example code for gql pre-approved queries

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
 import typeDefs from './graphql/schema';;
 import resolvers from './graphql/resolvers';
+import stringSimilarity from "string-similarity";
+import { print } from "graphql/language";
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+
+const approvedQueries = [
+  `
+  query CurrentUser($filter: UserPostsFilterInput) {
+    me {
+      id
+      role
+      name
+      email
+      createdAt
+      updatedAt
+      posts(filter: $filter) {
+        id
+        title
+        content
+        updatedAt
+        createdAt
+        publishedAt
+        isPublished
+      }
+    }
+  }
+  `
+];
 
 const port = 4000;
 const server = new ApolloServer({
   typeDefs, // our GraphQl schema definition
   resolvers, // schema resolvers
-  introspection: true // enables GraphIQl playground on the port the server is listening on
+  introspection: true, // enables GraphIQl playground on the port the server is listening on
+  plugins: [
+    {
+      requestDidStart: async () => ({
+        didResolveOperation: async ({ request, document }) => {
+          const query = print(document);
+          const operation = request.operationName || (document.definitions[0] as any).name?.value || 'unknown';
+
+          // It's possible that the queries are not identical due to differences in whitespace, comments, or other characters.
+          // Hence the use of `string-similarity` package
+          if (stringSimilarity.findBestMatch(query, approvedQueries).bestMatch.rating < 0.8) {
+            throw new Error(`Query "${operation}" not approved.`);
+          }
+        }
+      }),
+    }
+  ]
 });
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
